ece64629fd2fd34da871ba164d4bfaf58d3cde86
Carlos <miklo.concepcion@gmail.com>
2020-04-12 18:14:09 -0500

    footer styled. now I can forget about it. Onto nav styling

--- src/components/nav&footer/Footer.js
+++ src/components/nav&footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.scss";
 
 function Footer() {
-  return <div className='footer-container'>Footer Component.</div>;
+  return <div className='footer-container'>Patchy's Footer.</div>;
 }
 
 export default Footer;
