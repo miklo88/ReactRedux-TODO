78fe9c5154776d98b9d73627347bdc088f01587d
Carlos <miklo.concepcion@gmail.com>
2020-04-14 12:37:38 -0500

    cleaning up errors. starting to implement delete todo functionality.

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -10,6 +10,10 @@ export const setVisibilityFilter = (filter) => ({
   type: "SET_VISIBILITY_FILTER",
   filter,
 });
+//deleting a todo from the state
+export const removeTodo = (id) => {
+  type: "REMOVE_TODO", id;
+};
 // toggling the todo
 export const toggleTodo = (id) => ({
   type: "TOGGLE_TODO",
@@ -21,4 +25,5 @@ export const VisibilityFilters = {
   SHOW_ALL: "SHOW_ALL",
   SHOW_COMPLETED: "SHOW_COMPLETED",
   SHOW_ACTIVE: "SHOW_ACTIVE",
+  SHOW_REMOVED: "SHOW_REMOVED",
 };
