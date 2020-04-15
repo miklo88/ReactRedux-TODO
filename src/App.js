9e4ee868c3823f9f1f0c9a0b9323b1442ddce20c
Carlos <miklo.concepcion@gmail.com>
2020-04-15 15:05:21 -0500

    re-naming of my files in my folder might do the trick for heroku.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
+// components
+import Nav from "./Components/Nav_Footer/Nav";
+import MainContent from "./Components/Landing/MainContent";
+import Footer from "./Components/Nav_Footer/Footer";
 // styles
 import "./App.scss";
-// components
-import Nav from "./components/nav&footer/Nav";
-import MainContent from "./components/landing/MainContent";
-import Footer from "./components/nav&footer/Footer";
 
 function App() {
   return (
     <div className='App'>
       <Nav />
       <MainContent />
-      {/* <TodoCard /> */}
       <Footer />
     </div>
   );
