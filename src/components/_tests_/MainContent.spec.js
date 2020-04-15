ebd61c9e4e857b8fefee658f103ba509266adffe
Carlos <miklo.concepcion@gmail.com>
2020-04-15 14:49:57 -0500

    error from heroku says it cannot find a scss file when its clearly in the correct file path. I really have no clue with heroku at this point. so i'm creating a new pathys app from the cli and giving it a shot

--- src/components/_tests_/MainContent.spec.js
+++ src/components/_tests_/MainContent.spec.js
@@ -1,7 +1,8 @@
-import React from "react";
-import MainContent from "../landing/MainContent";
+// import React from "react";
+// import { render } from '@testing-library/react';
+// import MainContent from "../landing/MainContent";
 
-import patchy from "../../imgs/Exclude.png";
-import "../landing/MainContent.scss";
+// import patchy from "../../imgs/Exclude.png";
+// import "../landing/MainContent.scss";
 
-jest.mock("../landing/MainContent", () => () => "MainContent");
+// jest.mock("../landing/MainContent", () => () => "MainContent");
